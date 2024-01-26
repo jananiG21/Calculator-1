44281dc6829aa00a22a19ae8fd5ee1f8ac7d40d4
jananiG21 <kitcbe.25.21bcb021@gmail.com>
2024-01-26 07:21:50 +0000

    Calculator1 second commit

--- Calculator1.py
+++ Calculator1.py
@@ -7,13 +7,30 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+# Function to multiply two numbers
 
+def multiply(x, y):
+
+    return x * y
+
+
+# Function to divide two numbers
+
+def divide(x, y):
+
+    if y == 0:
+
+        return "Cannot divide by zero"
+
+    return x / y
 # Main program loop
 while True:
     # Display menu
     print("Options:")
     print("Enter 'add' for addition")
     print("Enter 'subtract' for subtraction")
+    print("Enter 'multiply' for multiplication")
+    print("Enter 'divide' for division")
     print("Enter 'quit' to end the program")
 
     # User input
@@ -24,7 +41,7 @@ while True:
         break
 
     # Check for valid operation
-    if user_input in ("add", "subtract"):
+    if user_input in ("add", "subtract","multiply", "divide"):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -32,5 +49,11 @@ while True:
             print("Result:", add(num1, num2))
         elif user_input == "subtract":
             print("Result:", subtract(num1, num2))
+        elif user_input == "multiply":
+            print("Result:", multiply(num1, num2))
+        elif user_input == "divide":
+            print("Result:", divide(num1, num2))
+
+
     else:
         print("Invalid input")
